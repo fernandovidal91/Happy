03f2750989a8de920eaa8a387c18146dd0931969
Fernando Angiolin Vidal <fernandovidal9@hotmail.com>
2020-10-28 21:26:42 -0300

    Next Level Week 3

---
+++ web/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Leaflet from 'leaflet';
+
+import 'leaflet/dist/leaflet.css';
+
+import mapMarkerImg from '../images/map-marker.svg';
+
+import "../styles/pages/orphanages-map.css";
+import api from '../services/api';
+
+const mapIcon = Leaflet.icon({
+    iconUrl: mapMarkerImg,
+
+    iconSize: [58, 68],
+
+    iconAnchor: [29, 68],
+
+    popupAnchor: [170, 2],
+})
+
+interface Orphanage {
+    id: number;
+    latitude: number;
+    longitude: number;
+    name: string;
+}
+
+function OrphanagesMap(){
+
+    const [orphanage, setOrphanages] = useState<Orphanage[]>([]);
+
+    useEffect(() => {
+        api.get('orphanages').then(response => {
+            setOrphanages(response.data);
+        });
+    }, []); 
+
+    return(
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Happy"/>
+
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                </header>
+
+                <footer>
+                    <strong>São Lourenço do Oeste</strong>
+                    <span>Santa Catarina</span>
+                </footer>
+            </aside>
+
+            <Map 
+                center={[-26.3560202,-52.8419929]}
+                zoom={15}
+                style={{ width: '100%', height: '100%'}}
+            >
+                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+                {orphanage.map(orphanage => {
+                    return(
+                        <Marker
+                        key={orphanage.id}
+                        icon={mapIcon}
+                        position={[orphanage.latitude, orphanage.longitude]}
+                    >
+
+                    <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                        {orphanage.name}
+                        <Link to={`/orphanages/${orphanage.id}`}>
+                            <FiArrowRight size={20} color="#FFF" />
+                        </Link>
+                    </Popup>
+
+                    </Marker>
+                    )
+                })}
+
+            </Map>
+
+            <Link to="/orphanages/create" className="create-orphanage">
+                <FiPlus size={32} color="#FFF" />
+            </Link>
+
+        </div>
+    );
+}
+
+export default OrphanagesMap;
